4419b779658b55402ec106f76ab77a276e9f4b58
Khusika Dhamar Gusti <mail@khusika.com>
2019-10-18 04:44:26 +0700

    Update toolchain
    tools: https://github.com/ClangBuiltLinux/tc-build/commits/439c648a4f70
    clang version: https://github.com/llvm/llvm-project/commits/1c4b5a8dfed8
    binutils version: 2.33.1
    clang target arch: ARM,AArch64,X86
    binutils target arch: ARM,AArch64
    
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "0399d5a9682b3cef71c653373e38890c63c4c365"
+#define LLVM_REVISION "1c4b5a8dfed853ca8cbee98f274ff0a173186586"
 #define LLVM_REPOSITORY "git://github.com/llvm/llvm-project"
